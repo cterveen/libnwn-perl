c3ec7efd4557f8665fc4644b06a6a6358f439871
Christiaan ter Veen <mail@rork.nl>
2021-01-22 19:52:35 +0100

    Removed all methods
    All methods are now available from NWN::2da which loads the specifications by default.

--- NWN/2da/spec/classes.pm
+++ NWN/2da/spec/classes.pm
@@ -293,55 +293,16 @@ sub new {
   bless($self, $class);
 }
 
-
-sub format {
-  my $self = shift;
-  my $header = shift;
-  
-  if (!exists($self->{header}->{$header}->{format})) {
-    return ["unknown header"];
-  }
-  return $self->{header}->{$header}->{format}; 
-}
-
-sub label {
-  my $self = shift;
-
-  return $self->{label};
-}
-
-sub maxrows {
-  my $self = shift;
-
-  return $self->{maxrows};
-}
-
-sub parent {
-  my $self = shift;
-
-  return $self->{parent};
-}
-
-sub reference {
-  my $self = shift;
-  my $header = shift;
-
-  if (!exists($self->{header}->{$header}->{reference})) {
-    return "";
-  }
-  return $self->{header}->{$header}->{reference};
-}
-
-
 =head1 NAME
 
-    NWN::2da::classes
+    NWN::2da::spec::classes
 
 =head1 SYNOPSIS
 
-    my $tda_classes = NWN::2da::classes->new()
+    my $spec = NWN::2da->new()
+       $spec->load("classes.2da");
 
-    print $tda_classes->{header}->{Name}->{description};
+    print $spec->label();
 
 =head1 DESCRIPTION
 
@@ -349,51 +310,12 @@ sub reference {
 
 =head1 METHODS
 
-=over
-
-=item format($header)
-
-    Returns an array reference consisting of the following items: [0] datatype
-    [1] test [2]... values for the test.
-
-    Datatype is one of string, int or bool. Test is either min, minmax, prefix
-    or choose. 
-
-    When test is min the next item in the list is the minimum value. For minmax
-    the next two values are the minumum and maximum respecively. When test is
-    prefix the next item is the prefix in lower case. On choose the next items
-    are the valid options in lower case.
-
-=item label()
-
-    Retuns a string with the header that is used as a label.
-
-=item maxrows()
-
-    Return a string with the maximum number of rows or 0 when there in no
-    maximum.
-
-=item parent()
-
-    Return a string with a reference to a parent file and a header:
-    {file}/{header}. If the file is a base file the string will be empty.
-
-=item reference($header)
-
-    Returns a string with a reference for the header. The reference can be one
-    of the following forms: 'tlk' as reference to the TLK table, '2da' as
-    reference to a 2da file, 'header{file}.2da', 'row/{file}.2da' as a
-    reference to a header or row in the given 2da file.
-
-
-  
-
-=back
+    Do not call this module directly but use NWN::2dac instead.
 
 =head1 MORE INFO
 
+    NWN::2da
     https://nwn.wiki/display/NWN1/classes.2da
-
 
 =cut
 
