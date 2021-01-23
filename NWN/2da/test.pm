c3a879a9e2a6167ab5bb87a3861edc9269dbeea3
Christiaan ter Veen <mail@rork.nl>
2021-01-23 13:06:52 +0100

    Headers, coding, POD
    Added tests for headers: checks the headers of the file against the headers in the specification and vice versa
    Changed a lot of the coding. All the tests are now written in test_* while the asserts test a value against expected. This should improve maintainability a lot.
    Updated the POD with the new methods, added headers for the methods, fixed a bug in the synopsis.

--- NWN/2da/test.pm
+++ NWN/2da/test.pm
@@ -27,10 +27,9 @@ sub set_error {
   $self->{fails}++;
 }
 
-
 sub errors {
   my $self = shift;
-  
+
   return @{$self->{errors}};
 }
 
@@ -49,7 +48,22 @@ sub fails {
 sub test_file {
   my $self = shift;
 
-  $self->assert_maxrows($self->rows, $self->maxrows());
+  # test the maximum number of rows
+  if ($self->maxrows() > 0) {
+    $self->assert_lesser_or_equal($self->rows, $self->maxrows(), "Error on the maximum number of rows:");
+  }
+
+  # test whether all headers are defined
+  my @headers = $self->headers();
+  my @defined_headers = keys(%{$self->{spec}->{header}});
+  foreach my $header(@headers) {
+    $self->assert_in_list($header, \@defined_headers, "Error undefined header in " . $self->{file} . ":");
+  }
+  
+  # test whether there are missing headers
+  foreach my $header(@defined_headers) {
+    $self->assert_in_list($header, \@headers, "Error missing header in " . $self->{file} . ":");
+  }
 }
 
 sub test_values {
@@ -62,203 +76,93 @@ sub test_values {
 
   foreach my $header(@headers) {
     my @col = $self->col($header);
-
     my $row = 0;
+
     foreach my $value(@col) {
-      $self->assert_format($value, $self->format($header), "Error on format for " . $header . "(" . $row . "):");
-      $self->assert_reference($self->reference($header), $value, "Error on reference for " . $header . "(" . $row . "):");
+
+      # ignore if empty (****)
+      if ($value eq "****") {
+        $row++;
+        next;
+      }
+
+      # test the format
+      my $message = "Error on format for " . $header . "(" . $row . "):";
+      my $format = $self->format($header);
+      my @format = ("foo");
+
+      if ($format->[0] eq "int") {
+        $self->assert_is_int($value, $message);
+      }
+      elsif ($format->[0] eq "float") {
+        $self->assert_is_float($value, $message);
+      }
+      elsif ($format->[0] eq "bool") {
+        $self->assert_is_bool($value, $message);
+      }
+      elsif ($format->[0] eq "string") {
+        $self->assert_is_string($value, $message);
+      }
+      elsif ($format->[0] eq "hex") {
+        $self->assert_is_hex($value, $message);
+      }
+
+      # test the value
+      $message = "Error in value for " . $header . "(" . $row . "):";
+      if (defined($format[1])) {
+        if ($format->[1] eq "min") {
+          $self->assert_more_or_equal($value, $format[2], $message);
+        }
+        if ($format->[1] eq "minmax") {
+          $self->assert_between_or_equal($value, $format[2], $message);
+        }
+        if ($format->[1] eq "prefix") {
+          $self->assert_prefix($value, $format[2], $message);
+        }
+        if ($format->[1] eq "choose") {
+          my @options;
+          foreach (2 .. scalar(@format)-1) {
+            push(@options, $format[$_]);
+          }
+          $self->assert_in_list($value, \@options, $message);
+        }
+      }
+
+      # test the reference
+      $message = "Error on reference for " . $header . "(" . $row . "):";
+      if ($self->reference($header) ne "") {
+        my @reference = split("/", $self->reference($header));
+
+        if ($reference[0] eq "2da") {
+          my @expected = sort(keys(%{$self->{siblings}}));
+          $self->assert_in_list(lc($value . ".2da"), \@expected, $message);
+        }
+        elsif ($reference[0] eq "row") {
+          my $tda = $self->load_2da($reference[1]);
+          if (defined($tda)) {
+            $self->assert_between_or_equal($value, 0, $tda->rows(-1), $message);
+          }
+        }
+        elsif ($reference[0] eq "header") {
+          my $tda = $self->load_2da($reference[1]);
+          if (defined($tda)) {
+            my @headers = $tda->headers();
+            $self->assert_in_list($value, \@headers, $message);
+          }
+        }
+        elsif ($reference[0] eq "col") {
+          my $tda = $self->load_2da($reference[1]);
+          if (defined($tda)) {
+            my @expected = $tda->col($reference[2]);
+            $self->assert_in_list($value, \@expected, $message);
+          }
+        }
+        # should add references for tlk, ltr and texture
+      }
+
       $row++;
     }
   }
-}
-
-sub assert_maxrows {
-  my $self = shift;
-  my $value = shift;
-  my $expected = shift;
-  my $message = shift;
-
-  $self->{tests}++;
-
-  if (!defined($message)) {
-    $message = "Error on the maximum number of rows:";
-  }
-  if ($expected == 0) {
-    return 1;
-  }
-  elsif ($value <= $expected) {
-    return 1;
-  }
-
-  $self->set_error($message . "\n  Expected:"  . $expected . "\n  Found:" . $value . "\n");
-  return 0;
-}
-
-sub assert_reference {
-  my $self = shift;
-  my $reference = shift;
-  my $value = shift;
-  my $message = shift;
-
-  if (!defined($message)) {
-    $message = "Reference not found:"
-  }
-
-  # ignore none references or empty values
-  if ($reference eq "" || $value eq "****") {
-    return 1;
-  }
-
-  $self->{tests}++;
-
-  my @reference = split("/", $reference);
-
-  if ($reference[0] eq "2da") {
-    if (!exists($self->{siblings}->{lc($value) . ".2da"})) {
-      my $expected = join("|", sort(keys(%{$self->{siblings}})));
-      if (length($expected) > 69) {
-        $expected = substr($expected, 0, 66) . "...";
-      }
-      $self->set_error($message . "\n  Expected: " . $expected . "\n  Found: " . lc($value) . ".2da\n");
-      return 0;
-    }
-  }
-  elsif ($reference[0] eq "row") {
-    my $tda = $self->load_2da($reference[1]);
-    if (defined($tda)) {
-      if ($value > $tda->rows()-1) {
-        $self->set_error($message . "\n  Expected: <" . ($tda->rows()-1) . " rows\n  Found: " . lc($value) . "\n");
-        return 0;
-      }
-    }
-    return 1;
-  }
-  elsif ($reference[0] eq "header") {
-    my $tda = $self->load_2da($reference[1]);
-    if (defined($tda)) {
-      my @headers = $tda->headers();
-      foreach my $header(@headers) {
-        if ($value eq $header) {
-          return 1;
-        }
-      }
-      my $expected = join("|", @headers);
-      if (length($expected) > 69) {
-        $expected = substr($expected, 0, 66) . "...";
-      }
-      $self->set_error($message . "\n  Expected: " . $expected . "\n  Found: " . $value . "\n");
-      return 0;
-    }
-  }
-  elsif ($reference[0] eq "col") {
-    my $tda = $self->load_2da($reference[1]);
-    if (defined($tda)) {
-      my @expected = $tda->col($reference[2]);
-      foreach my $expect(@expected) {
-        if ($value eq $expect) {
-          return 1;
-        }
-      }
-      my $expected = join("|", @expected);
-      if (length($expected) > 69) {
-        $expected = substr($expected, 0, 66) . "...";
-      }
-      $self->set_error($message . "\n  Expected: " . $expected . "\n  Found: " . $value . "\n");
-      return 0;
-    }
-  }
-  elsif ($reference[0] eq "texture") {
-    # to be implemented, but no idea where these are located
-  }
-  elsif ($reference[0] eq "tlk") {
-    # to be implemented, not sure whether that is needed as the number of
-    # items in the standard tlk table is very large (112010).
-  }
-  elsif ($reference[0] eq "ltr") {
-    # to be implemented, but no idea where these are located
-  }
-  else {
-    $self->set_error("Unknown reference:\n  Expected: 2da|row\n  Found: " . $reference[0] . "\n");
-  }
-}
-
-sub assert_format {
-  my $self = shift;
-  my $value = shift;
-  my $expected = shift;
-  my $message = shift;
-
-  if (!defined($message)) {
-    $message = "Error on value:"
-  }
-  
-  $self->{tests}++;
-
-  if ($value eq "****") {
-    return 1;
-  }
-
-  # check the datatypes
-  if ($expected->[0] eq "int") {
-    if ($value !~ m/^\-?\d+$/) {
-       $self->set_error($message . "\n  Expected:"  . $expected->[0] . "\n  Found: " . $value . "\n");
-      return 0;
-    }
-  }
-  elsif ($expected->[0] eq "float") {
-    if ($value !~ m/^\-?\d+(\.\d+)?$/) {
-      $self->set_error($message . "\n  Expected:"  . $expected->[0] . "\n  Found: " . $value . "\n");
-      return 0;
-    }
-  }
-  elsif ($expected->[0] eq "bool") {
-    if ($value != 0 && $value != 1) {
-      $self->set_error($message . "\n  Expected:"  . $expected->[0] . "\n  Found: " . $value . "\n");
-      return 0;
-    }
-  }
-  elsif ($expected->[0] eq "string") {
-    if ($value !~ m/^[\w_\(\)\-]+$/) {
-      $self->set_error($message . "\n  Expected:"  . $expected->[0] . "\n  Found: " . $value . "\n");
-      return 0;
-    }
-  }
-  elsif ($expected->[0] eq "hex") {
-    if ($value !~ m/^0x\d+$/) {
-      $self->set_error($message . "\n  Expected:"  . $expected->[0] . "\n  Found: " . $value . "\n");
-      return 0;
-    }
-  }
-  
-  # check the values
-  if (defined($expected->[1])) {
-    if ($expected->[1] eq "min" && $value < $expected->[2]) {
-      $self->set_error($message . "\n  Expected minumum of:"  . $expected->[2] . "\n  Found: " . $value . "\n");
-      return 0;
-    }
-    elsif ($expected->[1] eq "minmax" && ($value < $expected->[2] || $value > $expected->[3])) {
-      $self->set_error($message . "\n  Expected value between:"  . $expected->[2] . " and " . $expected->[3] . "\n  Found: " . $value . "\n");
-      return 0;
-    }
-    elsif ($expected->[1] eq "prefix" && lc(substr($value, 0, length($expected->[2]))) ne $expected->[2]) {
-      $self->set_error($message . "\n  Expected prefix:"  . $expected->[2] . " and " . $expected->[3] . "\n  Found: " . lc(substr($value, 0, length($expected->[2]))) . "\n");
-      return 0;
-    }
-    elsif ($expected->[1] eq "choose") {
-      my @options;
-      foreach (2 .. scalar(@{$expected})-1) {
-        push(@options, $expected->[$_]);
-      }
-      foreach my $option(@options) {
-        if (lc($value) eq $option) {
-          return 1;
-        }
-      }
-      $self->set_error($message . "\n  Expected one of: "  . join("|", @options) . "\n  Found: " . lc($value) . "\n");
-      return 0;
-    }
-  }
-  return 1;
 }
 
 sub load_2da {
@@ -296,6 +200,193 @@ sub siblings {
   }
 }
 
+sub assert_is_int {
+  my $self = shift;
+  my $value = shift;
+  my $message = shift;
+
+  if (!defined($message)) {
+    $message = "Error on check for integer:";
+  }
+
+  $self->{tests}++;
+
+  if ($value =~ m/^\-?\d+$/) {
+    return 1;
+  }
+  $self->set_error($message . "\nExpected: an integer\nFound: " . $value . "\n");
+}
+
+sub assert_is_float {
+  my $self = shift;
+  my $value = shift;
+  my $message = shift;
+
+  if (!defined($message)) {
+    $message = "Error on check for float:";
+  }
+
+  $self->{tests}++;
+
+  if ($value =~ m/^\-?\d+(\.\d+)?$/) {
+    return 1;
+  }
+  $self->set_error($message . "\nExpected: a float\nFound: " . $value . "\n");
+}
+
+sub assert_is_bool {
+  my $self = shift;
+  my $value = shift;
+  my $message = shift;
+
+  if (!defined($message)) {
+    $message = "Error on check for bool:";
+  }
+
+  $self->{tests}++;
+
+  if ($value =~ m/^[01]$/) {
+    return 1;
+  }
+  $self->set_error($message . "\nExpected: a bool\nFound: " . $value . "\n");
+}
+
+sub assert_is_string {
+  my $self = shift;
+  my $value = shift;
+  my $message = shift;
+
+  if (!defined($message)) {
+    $message = "Error on check for string:";
+  }
+
+  $self->{tests}++;
+
+  if ($value =~ m/^[\w_\(\)\-]+$/) {
+    return 1;
+  }
+  $self->set_error($message . "\nExpected: a string\nFound: " . $value . "\n");
+}
+
+sub assert_is_hex {
+  my $self = shift;
+  my $value = shift;
+  my $message = shift;
+
+  if (!defined($message)) {
+    $message = "Error on check for hex:";
+  }
+
+  $self->{tests}++;
+
+  if ($value =~ m/^0x[0-9a-f]+$/i) {
+    return 1;
+  }
+  $self->set_error($message . "\nExpected: a hex\nFound: " . $value . "\n");
+}
+
+sub assert_between_or_equal {
+  my $self = shift;
+  my $value = shift;
+  my $min = shift;
+  my $max = shift;
+  my $message = shift;
+
+  if (!defined($message)) {
+    $message = "Error on check whether value is between or equal:";
+  }
+
+  $self->{tests}++;
+
+  if ($value >= $min && $value <= $max) {
+    return 1;
+  }
+  $self->set_error($message . "\nExpected: ["  . $min . "-" . $max . "]\nFound: " . $value . "\n");
+  return 0;
+}
+
+sub assert_lesser_or_equal {
+  my $self = shift;
+  my $value = shift;
+  my $expected = shift;
+  my $message = shift;
+
+  if (!defined($message)) {
+    $message = "Error on check whether value is lower or equal than expected:";
+  }
+
+  $self->{tests}++;
+
+  if ($value <= $expected) {
+    return 1;
+  }
+  $self->set_error($message . "\nExpected <=: "  . $expected . "\nFound: " . $value . "\n");
+  return 0;
+}
+
+sub assert_more_or_equal {
+  my $self = shift;
+  my $value = shift;
+  my $expected = shift;
+  my $message = shift;
+
+  if (!defined($message)) {
+    $message = "Error on check whether value is more or equal than expected:";
+  }
+
+  $self->{tests}++;
+
+  if ($value >= $expected) {
+    return 1;
+  }
+  $self->set_error($message . "\nExpected >=: "  . $expected . "\nFound: " . $value . "\n");
+  return 0;
+}
+
+sub assert_in_list {
+  my $self = shift;
+  my $value = shift;
+  my $expected = shift;
+  my $message = shift;
+
+  if (!defined($message)) {
+    $message = "Error finding item in list:";
+  }
+
+  $self->{tests}++;
+
+   foreach my $expect (@{$expected}) {
+     if (lc($value) eq lc($expect)) {
+       return 1;
+     }
+   }
+
+   # trim expected list to 80 characters
+   my $expect = join("|", @{$expected});
+      $expect = substr($expect, 0, 67);
+   $self->set_error($message . "\nExpected one of: "  . $expect . "\nFound: " . lc($value) . "\n");
+   return 0;
+}
+
+sub assert_prefix {
+  my $self = shift;
+  my $value = shift;
+  my $expected = shift;
+  my $message = shift;
+
+  if (!defined($message)) {
+    $message = "Error on check for prefix:";
+  }
+
+  $self->{tests}++;
+
+  if (lc(substr($value, 0, length($expected))) eq lc($expected)) {
+    return 1;
+  }
+  $self->set_error($message . "\nExpected: "  . $expected . "*\nFound: " . $value . "\n");
+  return 0;
+}
+
 =head1 NAME
 
     NWN::2da::test
@@ -305,7 +396,8 @@ sub siblings {
     use NWN::2da::test;
     my $test = NWN::2da::test->new();
        $test->load($file);
-       $test->test_datatypes();
+       $test->test_file();
+       $test->test_values();
 
     print "Performed " . $test->tests() . " tests\n";
     print "Found " . $test->fails() . " errors\n";
@@ -316,25 +408,33 @@ sub siblings {
 
 =head1 METHODS
 
+=head2 Main methods
+
 =over
 
 =item load($file)
 
     Load a 2da file.
 
-=item test_files()
+=item test_file()
 
-     Tests the maximum number of rows.
+     Tests the file specifications. There are tests for the maxium number of
+     rows, missing headers and unknown headers.
 
 =item test_values()
 
-    Test whether the value of each item in a column is correct.
+    Test whether the value of each item in a column is correct. Empty (****)
+    values are ignored.
 
-    The format of the value is checked. This includes checks for minimum and/or
-    maximum values, file prefixes and preset options.
+    This checks the format which should be one of int, bool, string or hex.
+
+    The value itself can be checked for minimum and/or maximum values, prefixes
+    or preset options.
 
     If a value is a reference to another file there is a check that the file
-    and the reference exists.
+    and the reference exists. This currently supports references to other .2da
+    files only.
+
 
 =item tests()
 
@@ -350,30 +450,88 @@ sub siblings {
 
 =back
 
-=head1 PRIVATE METHODS
+=head2 ASSERTS
 
-    These methods have been implemented but are used primarily within the
-    module.
+    These methods do small checks of retrieved values against expected values.
+    A custom error message can be set, e.g. to refer to the origin of the value.
+
+    The assert function return 1 if the check is passed or set an error message
+    and return 0 if the check has failed.
 
 =over
 
-=item assert_maxrow($value, $expected, [$message])
+=item assert_between_or_equal($value, $min, $max, $message)
 
-    Assert the number of rows against the maximum number of rows.
+    Asserts wheter a value is between or equal to the set minimum and maxiumum.
 
-=item assert_value($value, $expected, [$message])
+=item assert_in_list($value, $list, $message)
 
-    Assert the value of a certain cell.
- 
+    Checks whether a value is in the list which is given as an array reference.
+
+=item assert_is_bool($value, $message)
+
+    Checks whether a value is a boolean (0 or 1)
+
+=item assert_is_float($value, $message)
+
+    Checks whether a value is a float number.
+    
+=item assert_is_hex($value, $message)
+
+    Checks whether a value has a hexadecimal format.
+
+=item assert_is_int($value, $message)
+
+    Checks whether a value is an integer
+
+=item assert_is_string($value, $message)
+
+    Checks whether a value is a string
+
+=item assert_lesser_or_equal($value, $max, $message)
+
+    Checks whether a value is lesser or equal to the given maximum.
+
+=item assert_more_or_equal($value, $min, $message)
+
+    Checks whether a value is more or equal to a given minimum.
+
+=item assert_prefix($value, $prefix, $message)
+
+    Checks whether a value starts with a given prefix.
+
+=back
+
+=head2 Internal methods
+
+    These methods are primarily used to load files, set errors etc.
+
+=over
+
+=item load_2da($filename)
+
+    Returns an object of another 2da file in the same base directory of the
+    tested file.
+
+=item set_error($message)
+
+    Adds the message to the list of errors and raises the number of fails with
+    one.
+
 =item siblings()
 
     Get a list of other 2da files in the same directory.
 
 =back
 
+=head2 Inherited methods
+
+    NWN::2da::test inherits all methods of NWN::2da.
+
 =head1 MORE INFO
 
-    See NWN::2da::spec::{class} for more information on the specifications.
+    NWN::2da 
+    NWN::2da::spec::{class}
 
 =cut
 1;
